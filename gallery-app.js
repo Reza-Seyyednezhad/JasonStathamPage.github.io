47a470c1b0bbbc5c01445637c43080a113dca8f4
Reza Seyyednezhad <Reza.Seyyed1379@gmail.com>
2022-02-26 22:14:44 +0330

    Final!!!

---
+++ gallery-app.js
@@ -0,0 +1,121 @@
+$('#one').on('click', function(e){
+    let getVal = document.querySelector('#one img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#two').on('click', function(e){
+    let getVal = document.querySelector('#two img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#three').on('click', function(e){
+    let getVal = document.querySelector('#three img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#four').on('click', function(e){
+    let getVal = document.querySelector('#four img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#five').on('click', function(e){
+    let getVal = document.querySelector('#five img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#six').on('click', function(e){
+    let getVal = document.querySelector('#six img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#seven').on('click', function(e){
+    let getVal = document.querySelector('#seven img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#eight').on('click', function(e){
+    let getVal = document.querySelector('#eight img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#nine').on('click', function(e){
+    let getVal = document.querySelector('#nine img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#ten').on('click', function(e){
+    let getVal = document.querySelector('#ten img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#eleven').on('click', function(e){
+    let getVal = document.querySelector('#eleven img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#twelve').on('click', function(e){
+    let getVal = document.querySelector('#twelve img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#thirteen').on('click', function(e){
+    let getVal = document.querySelector('#thirteen img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#fourteen').on('click', function(e){
+    let getVal = document.querySelector('#fourteen img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#fifteen').on('click', function(e){
+    let getVal = document.querySelector('#fifteen img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#sixteen').on('click', function(e){
+    let getVal = document.querySelector('#sixteen img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#seventeen').on('click', function(e){
+    let getVal = document.querySelector('#seventeen img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#eighteen').on('click', function(e){
+    let getVal = document.querySelector('#eighteen img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#nineteen').on('click', function(e){
+    let getVal = document.querySelector('#nineteen img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#twenty').on('click', function(e){
+    let getVal = document.querySelector('#twenty img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#twenty-one').on('click', function(e){
+    let getVal = document.querySelector('#twenty-one img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#twenty-two').on('click', function(e){
+    let getVal = document.querySelector('#twenty-two img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#twenty-three').on('click', function(e){
+    let getVal = document.querySelector('#twenty-three img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+$('#twenty-four').on('click', function(e){
+    let getVal = document.querySelector('#twenty-four img').src;
+    document.querySelector('.main-gallery-image').src = getVal;
+    e.preventDefault()
+})
+
